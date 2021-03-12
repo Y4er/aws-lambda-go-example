5e278ce7ea059de276642294e577653cd7639340
Y4er <45418382@qq.com>
2021-03-12 16:04:38 +0800

    Update main.go

--- main.go
+++ main.go
@@ -9,12 +9,11 @@ import (
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	fmt.Println(request.QueryStringParameters["id"])
-	fmt.Println(request.PathParameters["ips"])
 	return events.APIGatewayProxyResponse{
 		StatusCode:        200,
 		Headers:           map[string]string{"Content-Type": "text/plain"},
 		MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
-		Body:              "Hello, World!",
+		Body:              "Hello, World!"+request.QueryStringParameters["id"]+" "+request.path,
 		IsBase64Encoded:   false,
 	}, nil
 }
