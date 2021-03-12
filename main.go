d84a29701376904a661bdd09d846afd45f953a8d
Y4er <45418382@qq.com>
2021-03-12 16:19:23 +0800

    Update main.go

--- main.go
+++ main.go
@@ -8,17 +8,24 @@ import (
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	fmt.Println(request.QueryStringParameters["id"])
+	parameters := request.PathParameters
+	for p := range parameters {
+		fmt.Println(p)
+	}
+	body := fmt.Sprintf(
+		"Path:%s\nID:%s",
+		request.Path,
+		request.QueryStringParameters["id"],
+	)
 	return events.APIGatewayProxyResponse{
 		StatusCode:        200,
 		Headers:           map[string]string{"Content-Type": "text/plain"},
 		MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
-		Body:              "Hello, World!"+request.QueryStringParameters["id"]+" "+request.path,
+		Body:              body,
 		IsBase64Encoded:   false,
 	}, nil
 }
 
 func main() {
-	// Make the handler available for Remote Procedure Call by AWS Lambda
 	lambda.Start(handler)
 }
